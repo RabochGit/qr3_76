bd85b60e51ed00bca7d3fcc1b6ed5b201b85ed6d
Gruvi1 <asjj@gmail.com>
2023-10-27 21:31:55 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,98 @@
+import telebot
+from telebot import custom_filters
+from telebot import StateMemoryStorage
+from telebot.handler_backends import StatesGroup, State
+
+
+state_storage = StateMemoryStorage()
+# Вставить свой токет или оставить как есть, тогда мы создадим его сами
+bot = telebot.TeleBot('6533686655:AAFZUAyjtVcDLW_c1J-kE_zzIYmPYH7OGGI',
+                      state_storage=state_storage, parse_mode='Markdown')
+
+
+class PollState(StatesGroup):
+    name = State()
+    age = State()
+
+
+class HelpState(StatesGroup):
+    wait_text = State()
+
+
+text_poll = "опрос"  # Можно менять текст
+text_button_1 = "С грушей и горгонзолой"  # Можно менять текст
+text_button_2 = "Пепперони"  # Можно менять текст
+text_button_3 = "Четыре сыра"  # Можно менять текст
+
+
+menu_keyboard = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+menu_keyboard.add(
+    telebot.types.KeyboardButton(
+        text_poll,
+    )
+)
+menu_keyboard.add(
+    telebot.types.KeyboardButton(
+        text_button_1,
+    )
+)
+
+menu_keyboard.add(
+    telebot.types.KeyboardButton(
+        text_button_2,
+    ),
+    telebot.types.KeyboardButton(
+        text_button_3,
+    )
+)
+
+
+@bot.message_handler(state="*", commands=['start'])
+def start_ex(message):
+    bot.send_message(
+        message.chat.id,
+        'Привет! Пришёл сюда, чтобы научиться делать вкусную пиццу?',  # Можно менять текст
+        reply_markup=menu_keyboard)
+
+@bot.message_handler(func=lambda message: text_poll == message.text)
+def first(message):
+    bot.send_message(message.chat.id, 'Супер! *Ваше* *имя*?')  # Можно менять текст
+    bot.set_state(message.from_user.id, PollState.name, message.chat.id)
+
+
+@bot.message_handler(state=PollState.name)
+def name(message):
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['name'] = message.text
+    bot.send_message(message.chat.id, 'Супер! *Сколько* *вам* *лет*?')  # Можно менять текст
+    bot.set_state(message.from_user.id, PollState.age, message.chat.id)
+
+
+@bot.message_handler(state=PollState.age)
+def age(message):
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['age'] = message.text
+    bot.send_message(message.chat.id, 'Спасибо за регистрацию!', reply_markup=menu_keyboard)  # Можно менять текст
+    bot.delete_state(message.from_user.id, message.chat.id)
+
+
+@bot.message_handler(func=lambda message: text_button_1 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id, "[Пицца с грушей и горгонзолой!](https://www.edimdoma.ru/retsepty/58615-pitstsa-s-grushey-i-gorgonzoloy)", reply_markup=menu_keyboard)  # Можно менять текст
+
+
+@bot.message_handler(func=lambda message: text_button_2 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id, "[Пицца пепперони!](https://www.nur.kz/food/recipes/1773038-picca-pepperoni-sostav-i-recept/?ysclid=lnu9fdcub1168862266)", reply_markup=menu_keyboard)  # Можно менять текст
+
+
+@bot.message_handler(func=lambda message: text_button_3 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id, "[Пицца четыре сыра!](https://art-lunch.ru/recipe/pitstsa-4-syra_foto/?ysclid=lnu9gbad3x769531049)", reply_markup=menu_keyboard)  # Можно менять текст
+
+
+bot.add_custom_filter(custom_filters.StateFilter(bot))
+bot.add_custom_filter(custom_filters.TextMatchFilter())
+
+bot.infinity_polling()
+
